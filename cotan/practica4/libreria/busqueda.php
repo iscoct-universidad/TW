cbd164926c80f25f485279ee6be470acc242e85d
Cotan <iscoct@correo.ugr.es>
2019-05-05 13:53:10 +0200

    Terminada la practica 4

--- cotan/practica4/libreria/busqueda.php
+++ cotan/practica4/libreria/busqueda.php
@@ -1,10 +1,35 @@
 <?php
 	require_once './comun.php';
+	require_once './controller/BaseDeDatosLibreria.inc.php';
 	
 	$argumentos = [];
 	
 	addAutoresMasVentas($argumentos);
 	addUsuarioRegistrado($argumentos);
 	
-	renderizarPlantilla('./view/templates/busqueda.html', $argumentos);
+	if($_SERVER['REQUEST_METHOD'] == 'GET') {
+		$sql = "select genero from Libro";
+		
+		$generos = BaseDeDatosLibreria::consulta($sql);
+		$argumentos['generos'] = [];
+		
+		foreach($generos as $genero)
+			array_push($argumentos['generos'], $genero -> genero);
+	
+		renderizarPlantilla('./view/templates/busqueda.html', $argumentos);
+	} else if($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$sql = "select * from Libro where nombreAutor like '%" . $_POST['palabraBusqueda'] . "%' and titulo like '%" . $_POST['palabraBusqueda'] . "%'
+		and genero=\"" . $_POST['genero'] . "\";";
+		
+		$resultados = BaseDeDatosLibreria::consulta($sql);
+		$argumentos['coincidencias'] = [];
+		
+		foreach($resultados as $libro)
+			array_push($argumentos['coincidencias'], ['titulo' => $libro -> titulo,
+				'nombreAutor' => $libro -> nombreAutor, 'genero' => $libro -> genero]);
+		
+		renderizarPlantilla('./view/templates/mostrarResultadoBusqueda.html',
+			$argumentos);
+	} else
+		echo "Se ha equivocado de método";
 ?>
--- cotan/practica4/libreria/model/libro.sql
+++ cotan/practica4/libreria/model/libro.sql
@@ -4,16 +4,17 @@ create table if not exists Libro(
 	titulo varchar(64),
 	nombreAutor varchar(64),
 	editorial varchar(64),
+	genero varchar(64),
 	precio float
 );
 
 insert into Libro values ('1', './view/templates/img/libros/marina.jpg',
-	'Marina', 'Carlos Ruiz Zafon', 'deusto', '10'), ('2', 
+	'Marina', 'Carlos Ruiz Zafon', 'deusto', 'policial', '10'), ('2', 
 	'./view/templates/img/libros/angelesDemonios.jpg', 
-	'Angeles y Demonios', 'Dan Brown', 'deusto', '10'),
+	'Angeles y Demonios', 'Dan Brown', 'deusto', 'aventura', '10'),
 	('3', './view/templates/img/libros/idhun.jpg',
-	'Memorias de Idhun: La Resistencia', 'Laura Gallego', 'deusto', '20'),
+	'Memorias de Idhun: La Resistencia', 'Laura Gallego', 'deusto', 'ficcion', '20'),
 	('4', './view/templates/img/libros/inteligenciaEmocional.jpg', 
-	'Inteligencia Emocional', 'Daniel Goleman', 'deusto', '30'),
+	'Inteligencia Emocional de Daniel Goleman', 'Daniel Goleman', 'deusto', 'investigacion', '30'),
 	('5', './view/templates/img/libros/juegoDeTronos.jpg',
-	'Juego de Tronos: Cancion de Hielo y Fuego', 'George R. R. Martin', 'deusto', '20');
+	'Juego de Tronos: Cancion de Hielo y Fuego', 'George R. R. Martin', 'deusto', 'tragedia', '20');
